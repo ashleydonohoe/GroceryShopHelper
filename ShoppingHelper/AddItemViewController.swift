1151c23100a6f6e364cecf2ae1abeceab29a13fe
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-01-11 01:02:33 -0500

    Working on implementing image picking

--- ShoppingHelper/AddItemViewController.swift
+++ ShoppingHelper/AddItemViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
+class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var itemName: UITextField!
+    @IBOutlet weak var itemCategory: UITextField!
+    @IBOutlet weak var itemQuantity: UITextField!
+    @IBOutlet weak var itemPrice: UITextField!
+    @IBOutlet weak var itemImagePreview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +27,27 @@ class AddItemViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelItem(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    @IBAction func addImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    }
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    }
+    
+    @IBAction func saveItem(_ sender: Any) {
+        if let itemName = itemName.text, let itemCategory = itemCategory.text, let itemQuantity = Int(itemQuantity.text!), let itemPrice = Double(itemPrice.text!) {
+            
+            var newItem = Item(name: itemName, category: itemCategory, quantity: itemQuantity, price: itemPrice, image: nil, favorite: false)
+            print(newItem)
+        }
+    }
 }
---
+++ ShoppingHelper/ItemModel.swift
@@ -0,0 +1,20 @@
+//
+//  ItemModel.swift
+//  ShoppingHelper
+//
+//  Created by Ashley Donohoe on 1/10/17.
+//  Copyright © 2017 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Just a test struct for adding an item temporarily
+struct Item {
+    var name: String
+    var category: String
+    var quantity:Int
+    var price: Double
+    var image:UIImage? = nil //Image will be optional when user adds items
+    var favorite:Bool = false
+}
