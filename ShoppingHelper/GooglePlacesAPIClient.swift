3ca90bab7c5cee0b08e43026ee4286afd0f9ba8e
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-01-19 01:23:24 -0500

    adding API client file

---
+++ ShoppingHelper/GooglePlacesAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  GooglePlacesAPIClient.swift
+//  ShoppingHelper
+//
+//  Created by Ashley Donohoe on 1/19/17.
+//  Copyright © 2017 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+
+class GooglePlacesAPIClient: NSObject {
+    
+}
