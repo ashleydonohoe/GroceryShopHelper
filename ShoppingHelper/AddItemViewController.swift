34f539ee474d2b40cc99da7cae0b7494b51245ab
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-01-24 21:50:43 -0500

    preparing to try to fix hiding/showing keyboard

--- ShoppingHelper/AddItemViewController.swift
+++ ShoppingHelper/AddItemViewController.swift
@@ -91,3 +91,7 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
         return true
     }
 }
+
+extension AddItemViewController {
+    
+}
--- ShoppingHelper/CodeForLater.swift
+++ ShoppingHelper/CodeForLater.swift
@@ -9,50 +9,53 @@
 import Foundation
 import UIKit
 
-//override func viewWillAppear(_ animated: Bool) {
-//    super.viewWillAppear(animated)
-//    subscribeToKeyboardNotifications()
-//}
-//
-//override func viewWillDisappear(_ animated: Bool) {
-//    super.viewWillAppear(animated)
-//    unsubscribeFromKeyboardNotifications()
-//}
+extension AddItemViewController {
 
-//func keyboardWillShow(_ notification:Notification) {
-//    
-    //        if (!itemCategory.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation) && !(itemName.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation)))
-    //            {
-    //              view.frame.origin.y -= getKeyboardHeight(notification)
-    //        }
-    
-//    if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
-//        view.frame.origin.y -= getKeyboardHeight(notification)
-//    }
-//}
-//
-//func keyboardWillHide(_ notification:Notification) {
-//    //        if (!itemCategory.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation) && !(itemName.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation)))
-//    //        {
-//    //            view.frame.origin.y = 0
-//    //        }
-//    if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
-//        view.frame.origin.y -= getKeyboardHeight(notification)
-//    }
-//}
-//
-//func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-//    let userInfo = notification.userInfo
-//    let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-//    return keyboardSize.cgRectValue.height
-//}
-//
-//func subscribeToKeyboardNotifications() {
-//    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
-//    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
-//}
-//
-//func unsubscribeFromKeyboardNotifications() {
-//    NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
-//    NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
-//}
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+
+    func keyboardWillShow(_ notification:Notification) {
+        
+                if (!itemCategory.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation) && !(itemName.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation)))
+                    {
+                      view.frame.origin.y -= getKeyboardHeight(notification)
+                }
+        
+        if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+
+    func keyboardWillHide(_ notification:Notification) {
+        //        if (!itemCategory.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation) && !(itemName.isFirstResponder && !UIDeviceOrientationIsPortrait(UIDevice.current.orientation)))
+        //        {
+        //            view.frame.origin.y = 0
+        //        }
+        if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+}
